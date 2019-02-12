74b8e11045494c27f416e4a4dac7f7ae1f995702
Neil Berg <neil.berg14@gmail.com>
2019-02-11 21:46:24 -0800

    object destructuring

--- app1/src/components/Comment.js
+++ app1/src/components/Comment.js
@@ -5,11 +5,15 @@ import CommentDate from "./CommentDate";
 
 class Comment extends React.Component {
   render() {
+    const { user, text, date } = this.props.data;
     return (
       <div>
-        <UserInfo user={this.props.data.user} />
+        {/* <UserInfo user={this.props.data.user} />
         <CommentText text={this.props.data.text} />
-        <CommentDate date={this.props.data.date} />
+        <CommentDate date={this.props.data.date} /> */}
+        <UserInfo user={user} />
+        <CommentText text={text} />
+        <CommentDate date={date} />
       </div>
     );
   }
